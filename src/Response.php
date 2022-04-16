1446214a937020bf409a837b4bb2f3c5d00c0d91
Drajat Hasan <drajathasan20@gmail.com>
2022-04-16 08:27:10 +0700

    Add cache function to manage http cache

--- src/Response.php
+++ src/Response.php
@@ -3,19 +3,34 @@
  * @author Drajat Hasan
  * @email drajathasan20@gmail.com
  * @create date 2022-04-13 12:45:34
- * @modify date 2022-04-14 10:32:33
+ * @modify date 2022-04-16 08:26:12
  * @license GPLv3
  * @desc [description]
  */
 
 namespace Zein\Http;
 
+use DateTime;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class Response extends SymfonyResponse
 {
     private static $instance = null;
+    private array $cacheList = [
+        'must_revalidate'  => false,
+        'no_cache'         => false,
+        'no_store'         => false,
+        'no_transform'     => false,
+        'public'           => true,
+        'private'          => false,
+        'proxy_revalidate' => false,
+        'max_age'          => 600,
+        's_maxage'         => 600,
+        'immutable'        => true,
+        'last_modified'    => '',
+        'etag'             => '',
+    ];
     private array $scope = [
         'text' => 'text/plain',
         'html' => 'text/html',
@@ -29,12 +44,26 @@ class Response extends SymfonyResponse
         return self::$instance;
     }
 
+    public static function cache(array $inputedOptions = [])
+    {
+        $instance = static::getInstance();
+
+        // set last modified and etag
+        $instance->cacheList['last_modified'] = new DateTime;
+        $instance->cacheList['etag'] = hash('sha256', date('this'));
+
+        $instance->setCache(($instance->cacheList));
+
+        return $instance;
+    }
+
     public static function json($data, int $httpResponseCode = 200)
     {
-        $response = new JsonResponse;
-        $response->setStatusCode($httpResponseCode);
-        $response->setData($data);
-        $response->send();
+        $instance = static::getInstance();
+        $instance->headers->set('Content-Type', 'application/json');
+        $instance->setStatusCode($httpResponseCode);
+        $instance->setContent(json_encode($data));
+        $instance->send();
     }
 
     public static function __callStatic(string $method, array $paramter)
@@ -46,6 +75,20 @@ class Response extends SymfonyResponse
             if ($method === 'any' && is_callable($paramter[0])) exit($paramter[0](static::getInstance()));
 
             static::getInstance()->headers->set('Content-Type', $static->scope[$method]);
+            
+            if (isset($paramter[1]) && is_callable($paramter[1])) exit(call_user_func_array($paramter[1], [static::getInstance(), $paramter[0]]));
+
+            static::getInstance()->setContent($paramter[0])->send();
+        }
+    }
+
+    public function __call(string $method, array $paramter)
+    {
+        if (isset($this->scope[$method]))
+        {
+            if ($method === 'any' && is_callable($paramter[0])) exit($paramter[0](static::getInstance()));
+
+            static::getInstance()->headers->set('Content-Type', $this->scope[$method]);
             
             if (isset($paramter[1]) && is_callable($paramter[1])) exit(call_user_func_array($paramter[1], [static::getInstance(), $paramter[0]]));
 
--- test/response.php
+++ test/response.php
@@ -2,8 +2,11 @@
 
 use Zein\Http\Response;
 
+@error_reporting(-1);
+@ini_set('display_errors', true);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $object = new stdClass;
 $object->name = 'drajat';
-Response::json($object);
+Response::cache()->json($object);
